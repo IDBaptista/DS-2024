5b19e117ed623805dff18a5ce8b59b4c7f193c8f
Isabelly Baptista <165063355+IDBaptista@users.noreply.github.com>
2024-04-09 11:20:46 -0300

    Add files via upload

---
+++ Lógica de Programação e Algoritmos/Python/Atividade 26 - Função.py
@@ -0,0 +1,26 @@
+def soma (n1, n2):
+    return (n1+n2)
+
+print (soma(45, 48))
+a = soma (10, 20) / 5
+print (a)
+
+#Criar 4 funções com as operações matemáticas
+#subtração, divisão e multiplicação
+#teste depois as suas funções
+
+def subtracao (n1, n2):
+    return (n1-n2)
+
+print (subtracao(10, 38))
+
+def multiplicacao (n1, n2):
+    return (n1*n2)
+
+print (multiplicacao(74, 89))
+
+def divisao (n1, n2):
+    return (n1/n2)
+
+print (divisao(62, 37))
+

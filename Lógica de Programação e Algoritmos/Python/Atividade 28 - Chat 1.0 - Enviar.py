c86cfa38d1f0963d79554992e1b6eddd71b7376c
Isabelly Baptista <165063355+IDBaptista@users.noreply.github.com>
2024-05-14 15:08:11 -0300

    Add files via upload

---
+++ Lógica de Programação e Algoritmos/Python/Atividade 28 - Chat 1.0 - Enviar.py
@@ -0,0 +1,11 @@
+from click import clear
+import socket
+
+nome = input ("Qual o seu nome?")
+
+while True:
+    clear()
+    mensagem = input("Digite a sua mensagem: \n")
+    with open("chat_1.txt", "a") as arquivo:
+        arquivo.write (f"{nome} | {mensagem}")
+
